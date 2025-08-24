b3a9e3742ff2d4048e55ed6dca003a7a37ffb0bb
EfeSimsek1 <efe.simsek.ca@gmail.com>
2025-08-24 15:00:05 -0400

    test push

---
+++ Scripts/state.gd
@@ -0,0 +1,16 @@
+class_name State extends Object
+
+# true whenever this state is currently being exited, false if not
+var isExiting: bool
+
+func enter():
+	pass
+	
+func exit():
+	pass
+	
+func process():
+	pass
+	
+func physics_process():
+	pass
