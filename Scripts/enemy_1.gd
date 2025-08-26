75678006602fdaa58081312cef6ab9d9aa7d6ed9
TheBigBamer <dylanriwiclark@gmail.com>
2025-08-26 12:03:16 +0100

    melee enemy movement finished for now

--- Scripts/enemy_1.gd
+++ Scripts/enemy_1.gd
@@ -1,14 +1,35 @@
 extends CharacterBody2D
 
-@export var player: Node2D
+#@export var player: Node2D
 
-var direction = [Vector2(0, -1), Vector2(1, -1), Vector2(1, 0), Vector2(1, 1), Vector2(0, 1), Vector2(-1, 1), Vector2(-1, 0), Vector2(-1, -1)]
+#var direction = [Vector2(0, -1), Vector2(1, -1), Vector2(1, 0), Vector2(1, 1), Vector2(0, 1), Vector2(-1, 1), Vector2(-1, 0), Vector2(-1, -1)]
+var desired_vel = 0
+var max_steer_force = 10
 
+var speed = 10000
 
-var speed = 100
+@export var goal: Node = null
+
+func _ready() -> void:
+	$NavigationAgent2D.target_position = goal.global_position
+	$Timer.start()
 
 func _physics_process(delta: float) -> void:
-	var direction = (player.position-position).normalized()
-	velocity = direction * speed
-	look_at(player.position)
-	move_and_slide()
+	if !$NavigationAgent2D.is_target_reached():
+		var nav_point_direction = to_local($NavigationAgent2D.get_next_path_position()).normalized()
+		velocity = nav_point_direction * speed * delta
+		velocity = velocity + (seek(goal.global_position) * delta)
+		move_and_slide()
+
+
+func _on_timer_timeout() -> void:
+	if $NavigationAgent2D.target_position != goal.global_position:
+		$NavigationAgent2D.target_position = goal.global_position
+	$Timer.start()
+
+
+func seek(target: Vector2) -> Vector2:
+	var desired_velocity = (target - global_position).normalized() * speed
+	
+	var steering = (desired_velocity - velocity).limit_length(max_steer_force)
+	return steering
