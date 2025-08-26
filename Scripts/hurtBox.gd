cf7d594416756b837d79b26a2d17ea240956aa20
EfeSimsek1 <efe.simsek.ca@gmail.com>
2025-08-26 03:46:42 -0400

    improved previous bug fix

--- Scripts/hurtBox.gd
+++ Scripts/hurtBox.gd
@@ -1,9 +1,11 @@
 class_name HurtBox extends Area2D
 
 func _ready() -> void:
+	self.collision_layer = 0
+	self.collision_mask = 2
 	self.area_entered.connect(on_area_entered)
 	
-func on_area_entered(hit_box: Area2D) -> void:
-	if hit_box == null or hit_box is not HitBox: return 
+func on_area_entered(hit_box: HitBox) -> void:
+	if hit_box == null: return 
 	# TODO Deal damage
 	print("damage dealt")
