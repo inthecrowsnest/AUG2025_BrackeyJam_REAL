6b803936df9424097cdda99a94b16c9e92503469
EfeSimsek1 <efe.simsek.ca@gmail.com>
2025-08-26 03:33:40 -0400

    fixed a bug

--- Scripts/hurtBox.gd
+++ Scripts/hurtBox.gd
@@ -3,7 +3,7 @@ class_name HurtBox extends Area2D
 func _ready() -> void:
 	self.area_entered.connect(on_area_entered)
 	
-func on_area_entered(hit_box: HitBox) -> void:
-	if hit_box == null: return 
+func on_area_entered(hit_box: Area2D) -> void:
+	if hit_box == null or hit_box is not HitBox: return 
 	# TODO Deal damage
 	print("damage dealt")
--- Scripts/playerHurtBox.gd
+++ Scripts/playerHurtBox.gd
@@ -7,8 +7,9 @@ var hit_source: Node2D
 func _ready() -> void:
 	super()
 	
-func on_area_entered(hit_box: HitBox) -> void:
+func on_area_entered(hit_box: Area2D) -> void:
 	super(hit_box)
+	if hit_box == null or hit_box is not HitBox: return 
 	# TODO change this to hitbox.owner later
 	hit_source = hit_box
 	stateMachine.change_state(hurtState)
