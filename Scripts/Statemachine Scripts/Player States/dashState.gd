ebb34a3cfc3c4a21cc141998a3116e09cfa40e5f
jamin06 <jamin.gough@gmail.com>
2025-08-27 19:02:03 +0100

    Added health to character and added a basic main menu screen

--- Scripts/Statemachine Scripts/Player States/dashState.gd
+++ Scripts/Statemachine Scripts/Player States/dashState.gd
@@ -3,26 +3,37 @@ extends PlayerState
 @onready var moveState = $"../Move"
 @onready var idleState = $"../Idle"
 @onready var timer: Timer = $"../../Timer"
+@onready var delay_timer: Timer = $"../../DelayTimer"
 
 
 const DASH_Speed = 500
-const DASH_DURATION = 0.2
+var start = true
 
 func enter():
 	super()
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	parent.velocity = input_direction * DASH_Speed
 	
+	
 func process_input(delta) -> State:
 	super(delta)
 	var input_direction = Input.get_vector("left", "right", "up", "down")
-	timer.start()
+	if start:
+		timer.start()
 	if input_direction == Vector2.ZERO:
 		return idleState
+	else:
+		return moveState
 	return null
 	
 func exit():
 	super()
 
 func _on_timer_timeout() -> State:
+	delay_timer.start()
+	start = false
 	return moveState
+
+
+func _on_delay_timer_timeout() -> void:
+	start = true
--- Scripts/playerController.gd
+++ Scripts/playerController.gd
@@ -3,6 +3,9 @@ class_name Player extends CharacterBody2D
 @onready var state_machine = $StateMachine
 @onready var animator = $AnimationPlayer
 @onready var sprite = $Sprite
+@onready var healthBar = $healthBar
+
+var health: int = 10
 
 func _ready() -> void:
 	# Initialize the state machine, passing a reference of the player to the states,
@@ -15,6 +18,13 @@ func _unhandled_input(event: InputEvent) -> void:
 func _physics_process(delta: float) -> void:
 	state_machine.process_physics(delta)
 	move_and_slide()
+	healthBar.value = health
+	
+	if health <= 0:
+		die()
 
 func _process(delta: float) -> void:
 	state_machine.process_frame(delta)
+	
+func die():
+	get_tree().reload_current_scene()
