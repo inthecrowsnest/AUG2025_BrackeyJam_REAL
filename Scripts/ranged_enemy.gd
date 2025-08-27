e233926f6be9dcdc0c62babc32840e95b8011c3f
TheBigBamer <dylanriwiclark@gmail.com>
2025-08-27 15:19:27 +0100

    Ranged enemy movement and state set up

--- Scripts/ranged_enemy.gd
+++ Scripts/ranged_enemy.gd
@@ -1,44 +1,57 @@
 extends CharacterBody2D
 
-#@export var player: Node2D
-
-#var direction = [Vector2(0, -1), Vector2(1, -1), Vector2(1, 0), Vector2(1, 1), Vector2(0, 1), Vector2(-1, 1), Vector2(-1, 0), Vector2(-1, -1)]
-var desired_vel = 0
-var max_steer_force = 10
-
 var speed = 10000
 
-var target_found = false
+@onready var navigation_agent_2d: NavigationAgent2D = $NavigationAgent2D
+@onready var timer: Timer = $Timer
 
+var target_found = false
+var flee = false
 @export var goal: Node = null
 
 func _ready() -> void:
-	$NavigationAgent2D.target_position = goal.global_position
-	$Timer.start()
+	navigation_agent_2d.target_position = goal.global_position
+	timer.start()
 
 func _physics_process(delta: float) -> void:
-	if !$NavigationAgent2D.is_target_reached():
-		var next_path_position = $NavigationAgent2D.get_next_path_position()
+	if !navigation_agent_2d.is_target_reached():
+		var next_path_position = navigation_agent_2d.get_next_path_position()
 		var nav_point_direction = to_local(next_path_position).normalized()
-		if !target_found:
+		if !target_found and !flee:
 			velocity = nav_point_direction * speed * delta
-		else:
+		elif !target_found and flee:
+			velocity = -nav_point_direction * speed * delta
+		elif target_found and !flee:
 			velocity = Vector2.ZERO
-		#seek(next_path_position)
+		elif target_found and flee:
+			velocity = -nav_point_direction * speed * delta
+		
 		move_and_slide()
 
 
 func _on_timer_timeout() -> void:
-	if $NavigationAgent2D.target_position != goal.global_position:
-		$NavigationAgent2D.target_position = goal.global_position
-	$Timer.start()
+	if navigation_agent_2d.target_position != goal.global_position:
+		navigation_agent_2d.target_position = goal.global_position
+	timer.start()
 
 
 func seek(target: Vector2) -> void:
-	var direction = ($NavigationAgent2D.target_position - global_position).normalized()
+	var direction = (navigation_agent_2d.target_position - global_position).normalized()
 	rotation = direction.angle()
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	
 	target_found = true
+	print("found")
+
+
+
+
+
+func _on_flee_range_body_entered(body: Node2D) -> void:
+	flee = true
+
+
+func _on_attack_range_body_exited(body: Node2D) -> void:
+	target_found = false
+	flee = false
